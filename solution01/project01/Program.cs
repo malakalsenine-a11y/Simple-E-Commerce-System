60e01cc10348ef8b254b405a0e01547234958967
Malakaka <malakalsenine@gmail.com>
2026-05-03 14:33:14 +0400

    Update Simple E-Commerce System (Menu System)

--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -15,7 +15,29 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. View Product");
+                Console.WriteLine("3. Search Product");
+                Console.WriteLine("4. Add to Cart");
+                Console.WriteLine("5. View Cart");
+                Console.WriteLine("6. Checkout");
+                Console.WriteLine("7. Exit");
+
+                int choice = int .Parse (Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1: AddProduct(); break;
+                    case 2: ViewProducts(); break;
+                    case 3: /* Search */ break;
+                    case 4: /* Add to cart */ break;
+                    case 5: ViewCart(); break;
+                    case 6: Checkout(); break;
+                    case 7: return;
+                }
+            }
         }
     }
 }

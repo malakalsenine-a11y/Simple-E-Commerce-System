47ebe762d2e4b87181cbfc1102e580dddf35bc65
Malakaka <malakalsenine@gmail.com>
2026-05-03 14:20:53 +0400

    Update Simple E-Commerce System (Product Structure)

--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -1,7 +1,17 @@
 ﻿namespace project01
 {
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int Quantity;
+    }
+
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
